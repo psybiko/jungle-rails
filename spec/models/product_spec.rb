243d81be0d2f9e88135ffe5732868b1c94917a5b
Kc Kai <kirstenckai@gmail.com>
2020-05-06 21:19:47 +0000

    Finish product specs tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+    subject {
+      described_class.new(name: "test", price: 200, quantity: 5)
+    }
+
+    describe 'Validations' do
+      before(:each) do
+        @category = Category.new(id: 1)
+        subject.category = @category
+      end
+
+      after(:each) do
+        puts subject.errors.full_messages
+      end
+
+      it "is valid with valid attributes" do
+        expect(subject).to be_valid
+      end
+
+      it "is invalid with no name" do
+        subject.name = nil
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid with no price" do
+        subject.price_cents = nil
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid with no quantity" do
+        subject.quantity = nil
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid with no category" do
+        @category = nil
+        subject.category = @category
+        expect(subject).to be_invalid
+      end
+
+    end
+  end 
